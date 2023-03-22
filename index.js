6b379257c2d93843963bd91278e5e490b7e53154
Irina Nagirnaia <iriakisil@yandex.ru>
2023-03-23 00:06:12 +0300

    ревью испр

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-const editPopup = document.querySelector(".popup");
-const buttonEditPopup = document.querySelector(".profile__edit-button");
-const formInputName = document.querySelector('.popup__input-name');
-const formInputDescriptor = document.querySelector('.popup__input-descriptor');
+const editPopup = document.querySelector('.popup');
+const buttonEditPopup = document.querySelector('.profile__edit-button');
+const formInputName = document.querySelector('.popup__form-item_input_name');
+const formInputDescriptor = document.querySelector('.popup__form-item_input_descriptor');
 const buttonSaveEditPopup = document.querySelector('.popup__form');
 const userName = document.querySelector('.profile__user-name');
 const userDescription = document.querySelector('.profile__descriptor');
 
 buttonEditPopup.addEventListener('click', function() {
-  console.log('click')
 
   editPopup.classList.add('popup_opened');
 
@@ -18,7 +17,7 @@ buttonEditPopup.addEventListener('click', function() {
 const buttonCloseEditPopup = document.querySelector('.popup__close-button');
 
 buttonCloseEditPopup.addEventListener('click', function() { 
-  console.log('click')
+
   editPopup.classList.remove('popup_opened');
 })
 
